e39acf7324623e695a03e8e79d07c89b09678b4c
裕安 蔡 <a0909219938@gmail.com>
2025-07-29 16:02:57 +0800

    add
    修正網頁favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,18 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
   css: ['~/assets/css/tailwind.css'],
   postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
   },
   ssr: false, // 避免使用 SSR
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/TestNuxtPage/' : '/',
+      baseURL: process.env.NODE_ENV === 'production' ? '/TestNuxtPage/' : '/',
+      head: {
+          link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, // 預設格式
+          ]
+      }
   },
 })
